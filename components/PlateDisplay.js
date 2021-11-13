ce942e1489f04a58297575acf6df3f3e0898efdf
Alex Maldonado <alex@Alexs-MBP-3.home>
2021-11-13 14:59:22 -0800

    Add plate badges
    Added plate badges using badges from the native base library. Also added colors to the native base theme file.

--- components/PlateDisplay.js
+++ components/PlateDisplay.js
@@ -1,44 +1,52 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { PlateBadge } from './PlateBadge';
 
 function PlateDisplay({ plateNumbers }) {
   return (
     <View style={styles.plateNumbers}>
       {/* //Render 45lb plate */}
       {plateNumbers.fortyFive ? (
-        <Text style={styles.plateNumbersText}>
-          {plateNumbers.fortyFive + ': 45lbs'}
-        </Text>
+        <PlateBadge
+          text={'45 LB'}
+          number={plateNumbers.fortyFive}
+          bg={'plates.45'}
+        />
       ) : null}
-      {/* Render 35lb plate */}
+      {/* //Render 35lb plate */}
       {plateNumbers.thirtyFive ? (
-        <Text style={styles.plateNumbersText}>
-          {plateNumbers.thirtyFive + ': 35lbs'}
-        </Text>
+        <PlateBadge
+          text={'35 LB'}
+          number={plateNumbers.thirtyFive}
+          bg={'plates.35'}
+        />
       ) : null}
       {/* //Render 25lb plate */}
       {plateNumbers.twentyFive ? (
-        <Text style={styles.plateNumbersText}>
-          {plateNumbers.twentyFive + ': 25lbs'}
-        </Text>
+        <PlateBadge
+          text={'25 LB'}
+          number={plateNumbers.twentyFive}
+          bg={'plates.25'}
+        />
       ) : null}
+
       {/* //Render 10lb plate */}
       {plateNumbers.ten ? (
-        <Text style={styles.plateNumbersText}>
-          {plateNumbers.ten + ': 10lbs'}
-        </Text>
+        <PlateBadge text={'10 LB'} number={plateNumbers.ten} bg={'plates.10'} />
       ) : null}
+
       {/* //Render 5lb plate */}
       {plateNumbers.five ? (
-        <Text style={styles.plateNumbersText}>
-          {plateNumbers.five + ': 5lbs'}
-        </Text>
+        <PlateBadge text={'5 LB'} number={plateNumbers.five} bg={'plates.5'} />
       ) : null}
+
       {/* //Render 2.5lb plate */}
       {plateNumbers.twoPointFive ? (
-        <Text style={styles.plateNumbersText}>
-          {plateNumbers.twoPointFive + ': 2.5lbs'}
-        </Text>
+        <PlateBadge
+          text={'2.5 LB'}
+          number={plateNumbers.twoPointFive}
+          bg={'plates.2'}
+        />
       ) : null}
     </View>
   );
@@ -47,8 +55,9 @@ function PlateDisplay({ plateNumbers }) {
 const styles = StyleSheet.create({
   plateNumbersContainer: {},
   plateNumbers: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     height: 'auto',
     padding: 20,
     top: 30,
