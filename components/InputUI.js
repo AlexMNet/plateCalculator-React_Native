e1be43e3602859f0f54eeb407dc754b57e74ffe9
Alex Maldonado <alex@Alexs-MBP-2021.home>
2022-02-05 21:56:32 -0800

    Keyboard Dismiss Bug
    Fix bug that causes input not to return to original spot covering buttons. Use statusbar height in vertical offset property in keyboardavoidingview.

--- components/InputUI.js
+++ components/InputUI.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Ionicons } from '@expo/vector-icons';
 import InputAndCalcBtn from '../components/InputAndCalBtn';
-import { KeyboardAvoidingView, Platform, View } from 'react-native';
-import { Button, VStack, IconButton, Icon } from 'native-base';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 import { useHomeContext } from '../context/HomeProvider';
 import { useThemeContext } from '../theme/ThemeProvider';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 const InputUI = () => {
   const {
@@ -23,7 +22,8 @@ const InputUI = () => {
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={styles.inputWeightWrapper}
-      keyboardVerticalOffset={115}
+      //Get status bar height so elements return to their place after keyboard is dismissed
+      keyboardVerticalOffset={getStatusBarHeight() + 74}
     >
       <InputAndCalcBtn />
     </KeyboardAvoidingView>
